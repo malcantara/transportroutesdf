ccab95419d7504ab8bed244150487970192c11f6
Mario Alcantata <marioag49@gmail.com>
2013-10-27 23:34:04 -0600

    Se agrega compatibilidad con ActionBar. Se agrega y configura SearchWidget para implementar el mecanismo de busqueda.

--- src/com/discere/transportroutesdf/TransportRoutesActivity.java
+++ src/com/discere/transportroutesdf/TransportRoutesActivity.java
@@ -1,12 +1,17 @@
 package com.discere.transportroutesdf;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class TransportRoutesActivity extends ActionBarActivity {
 
- 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -14,13 +19,39 @@ public class TransportRoutesActivity extends ActionBarActivity {
 
   }
 
-
-
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.transport_routes, menu);
-    return true;
+
+    // Get the SearchView and set the searchable configuration
+    SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+    SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu
+        .findItem(R.id.action_search));
+    // Assumes current activity is the searchable activity
+    searchView.setSearchableInfo(searchManager
+        .getSearchableInfo(getComponentName()));
+    searchView.setIconifiedByDefault(false); // Do not iconify the widget;
+                                             // expand it by default
+
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+    case R.id.action_search:
+      searchLocation();
+      return true;
+
+    default:
+      return super.onOptionsItemSelected(item);
+    }
+
+  }
+
+  private void searchLocation() {
+
   }
 
 }
